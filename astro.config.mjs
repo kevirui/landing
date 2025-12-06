5b2f5bf2f9088c16e3f104b4b5348df5d165795c
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-12-06 01:31:13 -0300

    fix vercel error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 import { fileURLToPath } from 'url';
 import { resolve } from 'path';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
